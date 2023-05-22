69b7ef8af10945ed5d0d625c4bee25effe23805a
Luca Pisciotta <luca.pisciotta@lptech.io>
2023-05-22 15:15:18 +0200

    Add load balancer name prefix variable

--- variables.tf
+++ variables.tf
@@ -10,9 +10,9 @@ variable "drop_invalid_header_fields" {
   type        = bool
 }
 
-variable "logs_bucket_name" {
-  default     = null
-  description = "Bucket name for logs"
+variable "elb_security_policy" {
+  default     = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  description = "ELB security policy (https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html#describe-ssl-policies)"
   type        = string
 }
 
@@ -22,19 +22,19 @@ variable "is_internal" {
   type        = bool
 }
 
-variable "name" {
+variable "logs_bucket_name" {
+  default     = null
+  description = "Bucket name for logs"
+  type        = string
+}
+
+variable "name_prefix" {
   description = "Name of the load balancer"
   type        = string
   validation {
     condition     = can(regex("^[a-zA-Z0-9][a-zA-Z0-9-]{0,30}[a-zA-Z0-9]$", var.name))
     error_message = "Name must be between 2 and 32 characters long and can contain only alphanumeric characters and hyphens. Can't start or end with a hyphen."
   }
-}
-
-variable "elb_security_policy" {
-  default     = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  description = "ELB security policy (https://docs.aws.amazon.com/elasticloadbalancing/latest/application/create-https-listener.html#describe-ssl-policies)"
-  type        = string
 }
 
 variable "subnet_ids" {
